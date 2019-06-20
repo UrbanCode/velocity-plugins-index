8ce0aeeed0867bd9791c0e11c3f4aa1a9c6fab20
Adam Boe <adam.boe@hcl.com>
2019-06-20 14:45:46 -0400

    Validate Reading JSON

--- src/helpers/PluginsHelper.js
+++ src/helpers/PluginsHelper.js
@@ -1,10 +1,7 @@
 import fs from 'fs-extra'
-import log4js from 'log4js'
 import path from 'path'
 
 export const PLUGINS_DIR = path.join(__dirname, '../../plugins')
-const logger = log4js.getLogger('PluginHelper')
-logger.level = process.env.LOG_LEVEL || 'debug'
 
 export default class PluginsHelper {
   static async getJsonFromPluginFile(plugin, file) {
@@ -12,15 +9,13 @@ export default class PluginsHelper {
     try {
       stringContents = await fs.readFile(path.join(PLUGINS_DIR, plugin, file), 'utf-8')
     } catch (err) {
-      logger.error(`Could not read "${file}" file for plugin "${plugin}". Please ensure an info.json file exists in the directory "plugins/${plugin}" with the appropriate information.`)
-      throw err
+      throw new Error(`Could not read "${file}" file for plugin "${plugin}". Please ensure an info.json file exists in the directory "plugins/${plugin}" with the appropriate information.`, err)
     }
     let jsonContents
     try {
       jsonContents = JSON.parse(stringContents)
     } catch (err) {
-      logger.error(`Could not parse "${file}" file for plugin "${plugin}" as JSON. Please ensure the info.json file at "plugins/${plugin}/${file}" contains valid JSON with the appropriate information.`)
-      throw err
+      throw new Error(`Could not parse "${file}" file for plugin "${plugin}" as JSON. Please ensure the info.json file at "plugins/${plugin}/${file}" contains valid JSON with the appropriate information.`, err)
     }
     return jsonContents
   }
