3591fbd00ed8795166d7616f3193713983691639
Adam Boe <adam.boe@hcl.com>
2019-06-28 15:19:04 -0400

    Updated Plugin Error Wording

--- src/helpers/IndexHelper.js
+++ src/helpers/IndexHelper.js
@@ -8,8 +8,8 @@ import ReleasesHelper from './ReleasesHelper'
 export const FILE_NAME = 'index.json'
 export const FILE = path.join(__dirname, '../../', FILE_NAME)
 export const ERROR_TEXT = {
-  Root: 'must have root JSON object for index containing elements for each pluginId whose value is a JSON object containing elements for the plugins info along with a "current" element whose value is a release object containing the latest release of the plugin',
-  Plugin: 'must have string elements for each pluginId in the root JSON object whose value is a JSON object containing elements for the plugins info along with a "current" element whose value is a release object containing the latest release of the plugin',
+  Root: 'must have root JSON object for index containing elements for each pluginId whose values are a JSON object containing elements for the plugins info along with a "current" element whose value is a release object containing the latest release of the plugin',
+  Plugin: 'must have pluginId\'s as elements in the root JSON object whose values are a JSON object containing elements for the plugin\'s info along with a "current" element whose value is a release object containing the latest release of the plugin',
   Current: 'must have "current" element in each plugin object whose value is a release object'
 }
 
