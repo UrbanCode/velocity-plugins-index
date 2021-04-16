b425f34567f25ab4239def498779c2aca928312c
Gomathi A <a-gomathi@hcl.com>
2021-04-16 11:56:20 +0530

    Added auth for docker login

--- src/helpers/DockerHelper.js
+++ src/helpers/DockerHelper.js
@@ -49,7 +49,10 @@ export default class DockerHubHelper {
     }
     const options = {
       method: 'GET',
-      uri: `https://auth.docker.io/token?service=registry.docker.io&scope=repository%3A${encodeURIComponent(imageName)}%3Apull`,
+      uri: `https://auth.docker.io/token?service=registry.docker.io&scope=repository:${encodeURIComponent(imageName)}:pull`,
+      headers: { 
+        Authorization: 'Basic ' + Buffer.from(process.env.urbanCodeDocker_USR + ':' + process.env.urbanCodeDocker_PSW).toString('base64')
+      }
     }
     request(options, function (error, response, body) {
       if (error) {
